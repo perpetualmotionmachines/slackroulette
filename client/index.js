c18e2d2d10dc24978c099ddcfd2f0f8717e384cd
Tony Ito-Cole <tonyitocole@gmail.com>
2020-02-22 10:31:17 -0800

    Basic boilerplate compiled

---
+++ client/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app.jsx';
+// import SignUp from './components/SignUp.jsx';
+// import Login from './components/Login.jsx'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './stylesheets/styles.css';
+
+const Index = () => {
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <App />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+};
+
+ReactDOM.render(<Index />, document.getElementById('root'));
+
+
+//Add these routes once merged with Clara and Noah's code
+
+// <Route path="/signup">
+// <SignUp />
+// </Route>
+// <Route path="/login">
+// <Login />
+// </Route>
