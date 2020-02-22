b6bac8edf1249bc6b68b5c471cb8106833c90d4c
Tony Ito-Cole <tonyitocole@gmail.com>
2020-02-22 15:19:27 -0800

    Server info updated

--- server/routes/chatRoute.js
+++ server/routes/chatRoute.js
@@ -3,7 +3,18 @@ const router = express.Router();
 const Chat = require("../models/chatModel");
 
 router.get("/", (req, res) => {
-    
+    console.log('here');
+    Chat.find({}, (err, response) => {
+        if (err) {
+            return next({
+              log: `Express error handler caught an error getting chat logs: ${err}`,
+              status: 400,
+              message: { err: `${err}` }
+            });
+        }
+        res.locals.response = response;
+    }, (req, res) =>
+    res.status(200).json(res.locals.response))
 });
 
 module.exports = router;
