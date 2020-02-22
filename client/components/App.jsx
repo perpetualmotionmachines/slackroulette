7d09eb0fbcd34924deccf9ce13125e757797b344
Tony Ito-Cole <tonyitocole@gmail.com>
2020-02-22 12:23:17 -0800

    Sample ChatContainer

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import ChatContainer from './ChatsContainer.jsx'
 
 const App = () => {
   return (
     <React.Fragment>
     <h1>Hello</h1>
+    <div>
+    <ChatContainer />
+  </div>
 </React.Fragment>
   );
 };
