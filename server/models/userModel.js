0e6d302d24661ad403bf375edd85f89c6c2e83de
skang1004 <saejin.kang1004@gmail.com>
2020-02-22 13:01:30 -0800

    package files

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,61 +1,61 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const bcrypt = require("bcrypt");
-const mongoose = require("mongoose");
-const MONGO_URI = `mongodb+srv://eevee:eevee@cluster0-nphgk.mongodb.net/test?retryWrites=true&w=majority`;
-mongoose
-  .connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => console.log("Connected to Mongo DB."))
-  .catch(err => console.log(err));
+const bcrypt = require('bcrypt');
 
 const UserSchema = new Schema({
-  username: {
-    type: String,
-    trim: true,
-    required: true,
-    unique: true
-  },
-  email: {
-    type: String,
-    trim: true,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
+    username: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
 });
 const { username, password } = req.body;
 
-try {
-  //check if user exists
-  let user = await User.findOne({ username });
+UserSchema.pre('save', async function(next) {
+    const user = this;
+    if (username.isModified('password')) {
+        username.password = await bcrypt.hash(username.password, 8);
+    }
+    next();
+});
+// try {
+//     //check if user exists
+//     let user = await User.findOne({ username });
 
-  if (user) {
-    return res.status(400).json({ errors: [{ msg: "User already exists" }] });
-  }
+//     if (user) {
+//         return res
+//             .status(400)
+//             .json({ errors: [{ msg: 'User already exists' }] });
+//     }
 
-  user = new User({
-    username,
-    password
-  });
+//     user = new User({
+//         username,
+//         password
+//     });
 
-  //encrypt the password with bcrypt
-  const salt = await bcrypt.genSalt(10);
+//     //encrypt the password with bcrypt
+//     const salt = await bcrypt.genSalt(10);
 
-  user.password = await bcrypt.hash(password, salt);
+//     user.password = await bcrypt.hash(password, salt);
 
-  //saves the new user into database
-  await user.save();
-} catch (err) {
-  console.error(err.message);
-  res.status(500).send("Server error");
-}
+//     //saves the new user into database
+//     await user.save();
+// } catch (err) {
+//     console.error(err.message);
+//     res.status(500).send('Server error');
+// }
 
-module.exports = User = mongoose.model("user", UserSchema);
+module.exports = User = mongoose.model('user', UserSchema);
 // const mongoose = require('mongoose');
 // // TO DO: change crypto to bycrpt
 // const crypto = require('crypto');
