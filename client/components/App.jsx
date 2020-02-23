11031824da6c8ca70a6de5923e25fdce5fb38eff
Lance Jeffers <lancej1022@gmail.com>
2020-02-22 16:04:47 -0800

    finished login React component, created auth methods for frontend

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import ChatContainer from './ChatsContainer.jsx'
+import ChatContainer from './ChatsContainer';
 
 const App = () => {
-  return (
-    <React.Fragment>
-    <h1>Hello</h1>
-    <div>
-    <ChatContainer />
-  </div>
-</React.Fragment>
-  );
+    return (
+        <>
+            <h1>Hello</h1>
+            <div>
+                <ChatContainer />
+            </div>
+        </>
+    );
 };
 
 export default App;
