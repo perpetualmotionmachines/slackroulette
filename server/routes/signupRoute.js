b903fdf18d267aebaa5d1a3c7f58b281a7a70696
skang1004 <saejin.kang1004@gmail.com>
2020-02-22 16:05:07 -0800

    after signup, user gets redirected to chatscontainer component

--- server/routes/signupRoute.js
+++ server/routes/signupRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/userModel');
+const ChatsContainer = require('../../client/components/ChatsContainer');
 const signupController = require('../controllers/signupController');
 
 router.post('/', signupController.createUser, (req, res) => {
-    res.json({});
+    res.redirect(ChatsContainer);
 });
