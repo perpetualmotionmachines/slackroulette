768147134f1ae4e839efb182930213b58e175c7c
skang1004 <saejin.kang1004@gmail.com>
2020-02-23 13:35:08 -0800

    made password input unreadable

--- client/components/SignUp.jsx
+++ client/components/SignUp.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 const SignUp = () => {
     // declare state variables using hooks/useState
-    const [ inputs, setInputs ] = useState({
+    const [inputs, setInputs] = useState({
         email: '',
         username: '',
         password: ''
@@ -14,40 +14,59 @@ const SignUp = () => {
             email: inputs.email,
             username: inputs.username,
             password: inputs.password
-        }
-      fetch('/signup', {
+        };
+        fetch('/signup', {
             method: 'POST',
             headers: {
-              "Content-Type": "Application/JSON"
+                'Content-Type': 'Application/JSON'
             },
             body: JSON.stringify(body)
         });
-    }
-    
-  
+    };
+
     const { email, username, password } = inputs;
- 
+
     return (
         <main className="signUpContainer">
             <section className="signUpInfo">
                 <label>
                     Email:
-                    <input type="text" name="email" value={email} onChange={e => setInputs({email: e.target.value})} />
+                    <input
+                        type="text"
+                        name="email"
+                        value={email}
+                        onChange={e => setInputs({ email: e.target.value })}
+                    />
                 </label>
                 <label>
                     Username:
-                    <input type="text" name="username" value={username} onChange={e => setInputs({username: e.target.value})} />
+                    <input
+                        type="text"
+                        name="username"
+                        value={username}
+                        onChange={e => setInputs({ username: e.target.value })}
+                    />
                 </label>
                 <label>
                     Password:
-                    <input type="text" name="password" value={password} onChange={e => setInputs({password: e.target.value})} />
+                    <input
+                        type="password"
+                        name="password"
+                        value={password}
+                        onChange={e => setInputs({ password: e.target.value })}
+                    />
                 </label>
             </section>
-            <button type="submit" onClick={() => {handleSubmission()}}>
+            <button
+                type="submit"
+                onClick={() => {
+                    handleSubmission();
+                }}
+            >
                 Sign Up
             </button>
         </main>
-    )
-}
+    );
+};
 
 export default SignUp;
