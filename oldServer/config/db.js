b41d8ed56e493b0877d7243db2cad8a0ea5f1d90
Lance Jeffers <lancej1022@gmail.com>
2020-02-24 10:39:12 -0800

    deleted old files

--- oldServer/config/db.js
+++
@@ -1,2 +0,0 @@
-const SECRET = `mongodb+srv://eevee:eevee@cluster0-nphgk.mongodb.net/test?retryWrites=true&w=majority`;
-module.exports = SECRET;
--- oldServer/routes/loginRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-// const ChatsContainer = require('../../client/components/ChatsContainer');
-const verifyUserController = require('../controllers/verifyUserController');
-
-router.get('/', verifyUserController.verifyUser, (req, res) => {
-    res.redirect(ChatsContainer);
-});
